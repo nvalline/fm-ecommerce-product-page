4ee7d7213313a3362b73391aefc9e76c526cfcf0
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-10-30 11:27:45 -0600

    Implement style vars & favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					orange: 'hsl(26, 100%, 55%)',
+					paleOrange: 'hsl(25, 100%, 94%)'
+				},
+				neutral: {
+					veryDarkBlue: 'hsl(220, 13%, 13%)',
+					darkGrayishBlue: 'hsl(219, 9%, 45%)',
+					grayishBlue: 'hsl(220, 14%, 75%)',
+					lightGrayishBlue: 'hsl(223, 64%, 98%)',
+					white: 'hsl(0, 0%, 100%)'
+				},
+				shade: {
+					black: 'hsla(0, 0%, 0%, 0.75)'
+				}
+			},
+			fontFamily: {
+				sans: ['var(--font-kumbh-sans)']
+			}
+		}
+	},
+	plugins: []
 };
 export default config;
